ef96972420a9cb756a5bdbbeb98b2eee3028fb42
julian <illegal_burrito@live.com>
2022-03-11 09:45:18 -0800

    express api server up

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 // import files and packages up here
-
+const express = require('express');
+const morgan = require('morgan');
 
 // create your express server below
-var app;
-
+let app = express();
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello Express!");
+});
+
+app.get('/data', (req, res) => {
+    let data = require('./data.json');
+    res.status(200).send(data);
+})
 
 // finally export the express application
 module.exports = app;
