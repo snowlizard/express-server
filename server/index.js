6c91f9e8b56c81155a3278f72568d65bab42de76
julian <illegal_burrito@live.com>
2022-03-25 11:01:04 -0700

    port env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-// here we're importing your express app
-const server = require('./app');
+const server = require('./server');
 
-// We're telling the app to listen for network requests on port 3000
-server.listen(3000, function() {
-  console.log('Server is listening on http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+})
